9848c64189ade5807e97cae2e60d5e24aa6690c2
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-05-10 09:09:40 +0200

    Environment preparation

---
+++ specs.js
@@ -0,0 +1,17 @@
+/// <reference path="typings/jasmine/jasmine.d.ts" />
+'use strict';
+
+describe('javaScript', function () {
+    
+    beforeEach(function () {
+
+    });
+    
+    it('test', function () {
+        expect(true).toBe(true);
+    });
+
+});
+
+
+    
