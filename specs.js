170574c3a1c7f8997336a7266fbcfc40473824cb
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-05-12 08:39:33 +0200

    scope example

--- specs.js
+++ specs.js
@@ -2,13 +2,31 @@
 'use strict';
 
 describe('javaScript', function () {
-    
-    beforeEach(function () {
 
-    });
-    
-    it('test', function () {
-        expect(true).toBe(true);
+    it('scope tests', function () {
+        var itemA = 1;
+        
+        function f1(){
+            //expect(itemA === ).toBeTruthy();
+            var itemA = 2;
+            //expect(itemA === ).toBeTruthy();
+        };
+        
+        function f2(itemA){
+            //expect(itemA === ).toBeTruthy();
+            itemA = 3;
+            itemB = 4;
+            
+            var itemC = 5;
+        };
+        
+        f1();
+        //expect(itemA === ).toBeTruthy();
+        f2();
+        //expect(itemB === ).toBeTruthy();
+        //expect(itemA === ).toBeTruthy();
+        //expect(itemC === ).toBeTruthy();
+        var itemB;
     });
 
 });
