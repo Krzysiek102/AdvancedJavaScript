03ff7e7078faf10b74a7a8c3f34ea8128c98ecbb
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-06-09 06:47:36 +0200

    closure

--- specs.js
+++ specs.js
@@ -6,16 +6,16 @@ describe('javaScript', function () {
     it('scope', function () {
         var itemA = 1;
 
-        function f1(){
+        function f1() {
             expect(itemA === undefined).toBeTruthy();
             var itemA = 2;
             expect(itemA === 2).toBeTruthy();
         };
 
-        function f2(itemA){
+        function f2(itemA) {
             expect(itemA === undefined).toBeTruthy();
             itemA = 3;
-            itemB = 4;  
+            itemB = 4;
 
             var itemC = 5;
         };
@@ -30,14 +30,14 @@ describe('javaScript', function () {
     });
 
 
-    it('named function expression', function  (){
-        var functionExpression = function functionHiddenDeclaration (param1){
-            function innerFunction (){
-                if (param1 === 2){
+    it('named function expression', function () {
+        var functionExpression = function functionHiddenDeclaration(param1) {
+            function innerFunction() {
+                if (param1 === 2) {
                     return functionHiddenDeclaration(param1 - 1);
-                }else if (param1 === 1){
+                } else if (param1 === 1) {
                     return 1;
-                }else{
+                } else {
                     return param1;
                 }
             }
@@ -50,10 +50,10 @@ describe('javaScript', function () {
     });
 
 
-    it('IIFE', function() {
+    it('IIFE', function () {
         var itemA = 1;
         var itemB = 1;
-        (function f1(param1){
+        (function f1(param1) {
             expect(itemA === 1).toBeTruthy();
             expect(itemB === undefined).toBeTruthy();
             itemA = param1;
@@ -68,10 +68,10 @@ describe('javaScript', function () {
     });
 
 
-    it('let', function() {
+    it('let', function () {
         //expect(itemA === undefined).toBeTruthy();
         expect(itemB === undefined).toBeTruthy();
-        if (true){
+        if (true) {
             //expect(itemA === undefined).toBeTruthy();
             expect(itemB === undefined).toBeTruthy();
             let itemA = 1;
@@ -83,139 +83,233 @@ describe('javaScript', function () {
         expect(itemB === 1).toBeTruthy();
     });
 
-    it('hoisting', function  (){
+    it('hoisting', function () {
         var itemA = f1();
         var itemB = itemC;
         expect(itemA === 1).toBeTruthy();
         expect(itemB === undefined).toBeTruthy();
-        function f1 (){
+        function f1() {
             return 1;
         };
-        var itemC = function  (){
+        var itemC = function () {
             return f1();
         };
     });
 
 
-    it('this - implicit binding', function() {
-        function f1 (){
+    it('this - implicit binding', function () {
+        function f1() {
             //expect(this.itemA === 1).toBeTruthy();
         }
         var itemA = 1;
         f1();
 
-        function f2 (){
+        function f2() {
             expect(this.itemB === 2).toBeTruthy();
         };
         var itemB = 1;
-        var itemC = {itemB: 2, f4: f2};
+        var itemC = { itemB: 2, f4: f2 };
         itemC.f4();
     });
 
 
-    it('call - explicit binding', function() {
-        function f1 (){
+    it('call - explicit binding', function () {
+        function f1() {
             expect(this.itemA).toBe(2);
         }
 
         var itemA = 1;
-        var itemB = {itemA: 2};
+        var itemB = { itemA: 2 };
         f1.call(itemB);
     });
 
-    
-    it('call - explicit binding - hard binding example', function() {
-        function f1 (){
+
+    it('call - explicit binding - hard binding example', function () {
+        function f1() {
             expect(this.itemA === 1).toBeTruthy();
         }
 
-        var obj1 = {itemA: 1};
-        var obj2 = {itemA: 2};
+        var obj1 = { itemA: 1 };
+        var obj2 = { itemA: 2 };
 
         var f2 = f1;
-        f1 = function(){
+        f1 = function () {
             f2.call(obj1);
         }
 
         f1.call(obj2);
     });
 
-    it('call - explicit binding - dedicated bind function', function() {
-        function bind (fn, obj){
-            return function(){
+    it('call - explicit binding - dedicated bind function', function () {
+        function bind(fn, obj) {
+            return function () {
                 fn.call(obj);
             }
         }
-        function f1 (){
+        function f1() {
             expect(this.itemA === 1).toBeTruthy();
         }
 
-        var obj1 = {itemA: 1};
-        var obj2 = {itemA: 2};
+        var obj1 = { itemA: 1 };
+        var obj2 = { itemA: 2 };
 
         f1 = bind(f1, obj1);
 
         f1.call(obj2);
-    });    
+    });
 
-    
-    it('bind function from ES5', function() {
-        function f1 (){
+
+    it('bind function from ES5', function () {
+        function f1() {
             expect(this.itemA === 1).toBeTruthy();
         }
 
-        var obj1 = {itemA: 1};
-        var obj2 = {itemA: 2};
+        var obj1 = { itemA: 1 };
+        var obj2 = { itemA: 2 };
 
         f1 = f1.bind(obj1);
 
         f1.call(obj2);
     });
-        
-    it('this - new binding', function() {
-        function f1 (){
+
+    it('this - new binding', function () {
+        function f1() {
             var itemA = 1;
             expect(this.itemA === undefined).toBeTruthy();
         }
         var obj1 = new f1();
 
-        function f2 (){
+        function f2() {
             this.itemB = 1;
             expect(this.itemB === 1).toBeTruthy();
         }
         var obj2 = new f2();
     });
 
-    it('this - new vs hard binding', function() {
-        function f1 (){
+    it('this - new vs hard binding', function () {
+        function f1() {
             expect(this.itemA === undefined).toBeTruthy();
         }
 
-        var obj1 = {itemA: 1};
-        var obj2 = {itemA: 2};
+        var obj1 = { itemA: 1 };
+        var obj2 = { itemA: 2 };
 
         f1 = f1.bind(obj1);
 
         var obj3 = new f1();
     });
 
-    
-    it('closure', function() {
-        function f1 (){
+
+    it('closure', function () {
+        function f1() {
             var itemA = 1;
-            function f1Internal (){
+            function f1Internal() {
                 expect(itemA === 1).toBeTruthy();
             }
             f2(f1Internal);
         }
 
-        function f2(f2Internal){
+        function f2(f2Internal) {
             f2Internal();
-        } 
+        }
 
         f1();
     });
-            
+
+
+    it('closure set timeout', function (done) {
+        function f1() {
+            var itemA = 1;
+            setTimeout(function () {
+                expect(itemA === 1).toBeTruthy();
+                done();
+            }, 50);
+        }
+        f1();
+    });
+
+
+    it('closure set timeout in for loop with var', function (done) {
+        var sum = 0;
+        function f1() {
+            for (var i = 1; i <= 2; i++) {
+                setTimeout(function () {
+                    sum+=i;
+                }, 50);
+            }
+        }
+        f1();
+        setTimeout(function () {
+            expect(sum === 6).toBeTruthy();
+            done();
+        }, 100);
+    });  
+
+    it('closure set timeout in for loop with IIFE', function (done) {
+        var sum = 0;
+        function f1() {
+            for (var i = 1; i <= 2; i++) {
+                (function(i){
+                    setTimeout(function () {
+                        sum+=i;
+                    }, 50);                    
+                })(i);
+            }
+        }
+        f1();
+        setTimeout(function () {
+            expect(sum === 3).toBeTruthy();
+            done();
+        }, 100);
+    }); 
+
+    it('closure set timeout in for loop with let', function (done) {
+        var sum = 0;
+        function f1() {
+            for (let i = 1; i <= 2; i++) {
+                setTimeout(function () {
+                    sum+=i;
+                }, 50);
+            }
+        }
+        f1();
+        setTimeout(function () {
+            expect(sum === 3).toBeTruthy();
+            done();
+        }, 100);
+    });    
+
+    it('module pattern', function() {
+        var module1 = (function  module1Func(){
+            var privateItemA = 1;
+            return {
+                f1: function  (){
+                    return privateItemA;
+                }
+            }
+        })();
+
+        expect(module1.f1() === 1).toBeTruthy();
+    });
+
+    
+    it('module pattern returning api', function() {
+        var module1 = (function module1Func(){
+            var privateItemA = 1;
+            var api= {
+                f1: function  (){
+                    return privateItemA;
+                }
+            };
+            api.f2 = function  (){
+                    return 2;
+                }
+            return api;
+        })();
+
+        expect(module1.f1() === 1).toBeTruthy();
+        expect(module1.f2() === 2).toBeTruthy();
+    });
 
 });
 
